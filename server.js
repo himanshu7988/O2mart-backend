70c2ef3999ae8b94276cfcbdd6a0b12035fad740
himanshu7988 <himanshu.lodhi.aligarh@gmail.com>
2026-03-25 23:40:09 +0530

    some update

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.disable("x-powered-by");
 // ✅ BULLETPROOF CORS - Handles everything
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  // res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") return res.status(200).end();
   next();
